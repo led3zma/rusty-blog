2ca50a7829e34cd6d01a40eed7fb8e6df4a7fb83
Alejandro Salas <asalasledezma16@gmail.com>
2024-04-17 16:46:55 -0400

    VIEW: Initial config

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{middleware::Logger, web, App, HttpServer};
+use tera::Tera;
 
 use rusty_blog::controller::*;
 use rusty_blog::get_db_pool;
@@ -10,12 +11,14 @@ async fn main() -> std::io::Result<()> {
     let pool = get_db_pool();
 
     HttpServer::new(move || {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/*/*"));
         App::new()
             .service(routes::hello_world)
             .service(routes::get_posts)
             .service(routes::new_post)
             .wrap(Logger::default())
             .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(tera))
     })
     .bind(("0.0.0.0", 9900))?
     .run()
